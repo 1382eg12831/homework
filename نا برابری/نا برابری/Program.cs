5ec3e38c49941abf8ff87a82a952b07ed5d6b0f4
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-30 23:51:32 +0330

    Add files via upload

---
+++ نا برابری/نا برابری/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            int ali = 0, sum = 0;
+            for (int i = 0; i < 1000; i++)
+
+            {
+                sum = sum + i;
+
+                if (sum % 3 != 0 && sum % 5 != 0)
+                {
+                    Console.WriteLine(sum);
+
+
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
