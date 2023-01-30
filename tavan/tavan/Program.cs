c5e0f61802957fb9e33f029f44464b2ad2d075a8
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-31 00:10:59 +0330

    Add files via upload

---
+++ tavan/tavan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; Console.WriteLine("Enter your number: ");
+            n = Convert.ToInt32(Console.ReadLine()); tavan(n);
+        }
+        static void tavan(int n)
+        {
+            Console.WriteLine(Math.Pow(n, 2)); Console.WriteLine(Math.Pow(n, 3));
+        }
+    }
+}
