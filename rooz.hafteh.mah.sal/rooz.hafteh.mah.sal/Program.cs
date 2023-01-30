ad819a9b6bc93887337c0376053c98ad13758dbb
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-30 23:48:13 +0330

    Add files via upload

---
+++ rooz.hafteh.mah.sal/rooz.hafteh.mah.sal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            day = Convert.ToInt32(Console.ReadLine());
+            Age(day);
+
+        }
+        private static void Age(int day)
+        {
+            int year, month, week;
+            year = day / 365;
+            day %= 365; month = day / 30;
+            day %= 30;
+            week = day / 7; day %= 7;
+            Console.WriteLine(year); Console.WriteLine(month);
+            Console.WriteLine(week); Console.WriteLine(day);
+        }
+
+
+
+
+    }
+}
