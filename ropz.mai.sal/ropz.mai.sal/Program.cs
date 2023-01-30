3c265f4003029ef6f34bbf91a6181b18d8a7d4d3
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-31 00:09:17 +0330

    Add files via upload

---
+++ ropz.mai.sal/ropz.mai.sal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime birthDay = new DateTime();
+            DateTime today = DateTime.Today;
+
+            Console.WriteLine("Enter your birthday like : 1379/01/01");
+            birthDay = Convert.ToDateTime(Console.ReadLine());
+            TimeSpan age = today.Subtract(birthDay);
+            Console.WriteLine(age);
+            int year = age.Days / 365;
+            int month = (age.Days % 365) / 30;
+            int day = (age.Days % 365) % 30;
+            Console.WriteLine($"you have {year} year  nad {month} month and {day} days old");
+            Console.ReadLine();
+        }
+    }
+}
