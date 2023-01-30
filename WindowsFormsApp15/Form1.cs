93201552e4139736194d209df774a981bc3b4309
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-30 23:40:45 +0330

    Add files via upload

---
+++ WindowsFormsApp15/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double avg;
+            avg = (a * 0.35) + (b * 0.65);
+            if (a>0 && a<=20&& b>0&& b<=20)
+            {
+                MessageBox.Show(avg.ToString());
+            }
+            else
+            {
+                MessageBox.Show("no");
+            }
+        }
+    }
+}
