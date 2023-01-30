cf3995f64ebb49173e715252beef895a46a32695
1382eg12831 <119557639+1382eg12831@users.noreply.github.com>
2023-01-30 23:46:23 +0330

    Add files via upload

---
+++ hajm 1/hajm 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x;
+            Console.WriteLine("enter number1 :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2 :");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number3 :");
+            c = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine(Volom(a, b, c));
+
+        }
+        private static int Volom(int a, int b, int c)
+        {
+            return a * b * c;
+
+        }
+    }
+
+}
